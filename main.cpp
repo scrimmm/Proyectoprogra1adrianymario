44643d6e4399708de12b9a4a018e6432baa2b667
scrimmm <adrianmedinabarrientos@gmail.com>
2016-08-21 12:24:08 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	Pelicula peli1("Mario come pinga","porno","Doblada","VIP","Adultos +18", "3D");
+	Pelicula peli1("Mario come pinga","porno","Doblada","VIP","Adultos +18", "3D", "asgfisadgoiasjgpasoifnsaoidmaspomfpsaokdmopsajkdpoaskfpoashfpiasjdpiasnbfipsajmdop");
 	
 	cout<<peli1.toString();
 }
