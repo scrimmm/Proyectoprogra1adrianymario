2ef815071e3e9b924f7f83ef8d2010e67d970a64
Manyo12 <mariochaves1292@gmail.com>
2016-08-21 10:49:31 -0600

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include "Coleccion.h"
+
+int main ()
+{
+	Coleccion s1;
+	
+	cout << s1.toString();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Persona.cpp
@@ -0,0 +1,83 @@
+#include "Persona.h"
+
+Persona::Persona(string N, string A1, string A2, int E) 
+{
+	Nombre = N;
+	Apellido1 = A1;
+	Apellido2 = A2;
+	Edad = E;
+}
+
+Persona::~Persona() 
+{
+	
+}
+
+void Persona::setNombre(string N)
+{
+	Nombre = N;
+}
+
+void Persona::setApellido1(string A)
+{
+	Apellido1 = A;
+}
+
+void Persona::setApellido2(string A)
+{
+	Apellido2 = A;
+}
+
+void Persona::setEdad(int E)
+{
+	Edad = E;
+}
+
+string Persona::getNombre()
+{
+	return Nombre;
+}
+
+string Persona::getApellido1()
+{
+	return Apellido1;
+}
+
+string Persona::getApellido2()
+{
+	return Apellido2;
+}
+
+int Persona::getEdad()
+{
+	return Edad;
+}
+
+string Persona::EsMay()
+{
+	int x;
+	while (Edad < 0)
+	{
+		cout << "Error de edad, digite una nueva" << endl;
+		cin >> x;
+		setEdad(x);
+	}
+	
+	if ((Edad >= 0) && (Edad < 18))
+	{
+		return "Menor";
+	}
+		
+	else 
+		return "Mayor";
+	
+}
+
+string Persona::toString()
+{
+	stringstream s;
+	
+	s << "El nombre del cliente es: " << Nombre << " " << Apellido1 << " " << Apellido2 << endl;
+	
+	return s.str();
+}
