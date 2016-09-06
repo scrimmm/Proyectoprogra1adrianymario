91f6f11c0d239c891086a18621116b9b2f695975
Manyo12 <mariochaves1292@gmail.com>
2016-09-05 23:31:15 -0600

    Add files via upload

---
+++ Nodo.cpp
@@ -0,0 +1,41 @@
+#include "Nodo.h"
+
+Nodo::Nodo(Pelicula *d) 
+{
+	dato = d;
+	Siguiente = NULL;
+}
+
+Nodo::Nodo (Pelicula *d, Nodo *s)
+{
+	dato = d;
+	Siguiente = s;
+}
+
+void Nodo::setDato(Pelicula *d)
+{
+	dato = d;
+}
+
+void Nodo::setSiguiente (Nodo *s)
+{
+	Siguiente = s;
+}
+
+Pelicula *Nodo::getPel()
+{
+	return dato;
+}
+
+Nodo *Nodo::getSiguiente()
+{
+	return Siguiente;
+}
+
+
+Nodo::~Nodo() 
+{
+	
+}
+
+
